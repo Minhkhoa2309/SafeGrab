cebf136fb91e3fd2b48bcd007bdacad14504f0b9
KevinTheDude511 <d.khuong0511@gmail.com>
2024-05-16 22:37:07 +0700

    add security package for hosting

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 require("dotenv").config();
 
+// Security package
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+
+// ExpressJS package
 const express = require("express");
 const app = express();
 const { connectDatabase } = require("./database/database");
+
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // 100 requests per windowMs
+}))
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
 
 const routers = [
     "speedsRouter",
@@ -15,6 +30,10 @@ const routers = [
 routers.forEach((routerName) => {
     const router = require(`./routes/${routerName}`);
     app.use("/api/v1", router);
+});
+
+app.use("/", (req, res) => {
+    res.status(200).send("Connection successful");
 });
 
 const port = process.env.PORT || 5000;
