9e28e9a49df980cacdd478050e47e89fffc4a81c
dat22 <buidatlc123@gmail.com>
2024-05-22 23:24:54 +0700

    Add trigger update_geohash_on_insert

--- data/create_table.py
+++ data/create_table.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, TIMESTAMP
 from sqlalchemy import UniqueConstraint, text
+from sqlalchemy.dialects.postgresql import VARCHAR
 
 # Load environment variables from .env file
 load_dotenv()
@@ -61,15 +62,40 @@ crashes_table = Table(
     Column('street_name', String),
     Column('injuries_total', Integer),
     Column('latitude', Float),
-    Column('longitude', Float)
+    Column('longitude', Float),
+    Column('geom', geometry(Point, 4326))
 )
 
 # Create tables
 metadata.create_all(engine)
 
 with engine.connect() as connection:
-    connection.execute(text("ALTER TABLE crashes ADD COLUMN geom geometry(Point, 4326);"))
-    connection.execute(text("UPDATE crashes SET geom = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326);"))
+    connection.execute(text("""
+        CREATE OR REPLACE FUNCTION calculate_geohash(latitude float, longitude float)
+        RETURNS varchar(12) AS $$
+        BEGIN
+            RETURN ST_SetSRID(ST_MakePoint(longitude, latitude), 4326);
+        END;
+        $$ LANGUAGE plpgsql;
+    """))
+    
+    connection.execute(text("""
+        CREATE OR REPLACE FUNCTION update_geohash_before_insert()
+        RETURNS TRIGGER AS $$
+        BEGIN
+          NEW.geom := calculate_geohash(NEW.latitude, NEW.longitude);
+          RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+    """))
+
+    connection.execute(text("""
+        CREATE TRIGGER update_geohash_on_insert
+        BEFORE INSERT ON crashes
+        FOR EACH ROW
+        EXECUTE PROCEDURE update_geohash_before_insert();
+    """))
+
     connection.execute(text("CREATE INDEX idx_crashes_geom ON crashes USING GIST (geom);"))
 
 print("Tables created successfully!")
